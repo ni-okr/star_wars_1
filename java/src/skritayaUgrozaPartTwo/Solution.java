0f0f285f04609d247e54d15ccb1afa635a48137c
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 13:40:37 +0300

    Эпизод 1: Скрытая угроза. Часть 2 Магистр Йода

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,13 +1,11 @@
 package skritayaUgrozaPartTwo;
 
 public class Solution {
-    public static int a = 1;
-    public static int b = 3;
-    public static int c = 9;
-    public static int d = 27;
-
     public static void main(String[] args) {
-        int result = -a + b - c + d;
-        System.out.println(result);
+        System.out.println(sqr(5));
+    }
+
+    public static int sqr(int a) {
+        return a * a;
     }
 }
