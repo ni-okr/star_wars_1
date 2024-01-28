0fa9e15c13cd8d1c8f269062122eba4ca7873290
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 12:23:18 +0300

    Эпизод 1: скрытая угроза. Часть 1 Подводный город Ото-Гунга

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Поздравляю товарища Ким Чен Ына с успешным запуском в японском море!");
+        System.out.println("Поздравляю товарища Ким Чен Ына\n с успешным запуском\n в японском море!");
     }
 }
--- java/src/skritayaUgrozaPartOne/Gungans.java
+++ java/src/skritayaUgrozaPartOne/Gungans.java
@@ -4,6 +4,6 @@ public class Gungans {
     String name = "Jar-Jar";
 
     void display_info() {
-        System.out.println("Name: %s", name);
+        System.out.println("Name: %s" + name);
     }
 }
