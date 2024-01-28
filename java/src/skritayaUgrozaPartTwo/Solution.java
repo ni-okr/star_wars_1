8a2bf74b983e9a548dc9596063dc981a89e36042
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 13:22:42 +0300

    Эпизод 1: Скрытая угроза. Часть 2 Галактический сенат

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -2,7 +2,8 @@ package skritayaUgrozaPartTwo;
 
 public class Solution {
     public static void main(String[] args) {
-        String jedi = "Jedi";
-        System.out.println(jedi);
+        int a = 1;
+        int b = 2;
+        String s = "str";
     }
 }
