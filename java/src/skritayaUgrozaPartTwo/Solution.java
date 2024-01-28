cac5ae1cb48c5a486e2e002cb14fdd048517144f
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 15:39:10 +0300

    Эпизод 2: Атака клонов. Часть 2 Неуловимый Фетт

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,29 +1,11 @@
 package skritayaUgrozaPartTwo;
 
 public class Solution {
-    public static class Dias {
-    }
-
-    public static class Clone1 {
-        public Dias owner;
-    }
-
-    public static class Clone2 {
-        public Dias owner;
-    }
-
-    public static class Clone3 {
-        public Dias owner;
+    public static double getWeight(int weight) {
+        return (weight * 0.17);
     }
 
     public static void main(String[] args) {
-        Clone1 clone1 = new Clone1();
-        Clone2 clone2 = new Clone2();
-        Clone3 clone3 = new Clone3();
-        Dias dias = new Dias();
-
-        clone1.owner = dias;
-        clone2.owner = dias;
-        clone3.owner = dias;
+        System.out.println(getWeight(800));
     }
 }
