5ef24c3b1525c93ae1c12266872f8e2a6301f166
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 15:01:28 +0300

    Эпизод 2: Атака клонов. Часть 2 Зам Вессел

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,11 +1,24 @@
 package skritayaUgrozaPartTwo;
 
 public class Solution {
-    static void increaseSpeed(int speed){
-        System.out.println("Your speed is: " + (speed + 100) + " km/h.");
+    public static class Zam {
+        public int age;
+        public int height;
+        public Dron spy;
+    }
+
+    public static class Dron {
+        public int version;
+        public int weight;
+        public Zam hunter;
+
     }
 
     public static void main(String[] args) {
-        increaseSpeed(700);
+        Zam zam = new Zam();
+        Dron dron = new Dron();
+
+        dron.hunter = zam;
+        zam.spy = dron;
     }
 }
