9a93268388db9e062b257140fadb608daf587851
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 16:36:03 +0300

    Эпизод 2: Атака клонов. Часть 1 Преследование продолжается

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,17 +1,23 @@
 package skritayaUgrozaPartTwo;
 
 public class Solution {
-    public static int min(int a, int b) {
-        if (a < b){
-            return b;
-        } else {
+    public static int max(int a, int b, int c, int d) {
+        return (max(max(a, b), max(c, d)));
+    }
+
+    public static int max(int a, int b) {
+        if (a > b) {
             return a;
+        } else {
+            return b;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(min(12, 33));
-        System.out.println(min(-20, 0));
-        System.out.println(min(-10, -20));
+//        System.out.println(max(-20, -10));
+        System.out.println(max(-40, -10, -30, 40));
+        System.out.println(max(-20, -40, -30, 40));
+        System.out.println(max(-20, -10, -40, 40));
+        System.out.println(max(-20, -10, -30, -40));
     }
 }
