707e44ed2847135f50d808c974bd04eecefc47a2
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 12:48:00 +0300

    Эпизод 1: скрытая угроза. Часть 1 Схватка с Дартом Молом

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("It's blue side");
+        String str = "See you next time";
+        System.out.printf("%s", str);
     }
 }
