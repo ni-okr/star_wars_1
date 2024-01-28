7fd3d76af8ce89101d30c0235decdd0c39c7a3fc
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 13:30:30 +0300

    Эпизод 1: Скрытая угроза. Часть 2 Герой Энакин

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -2,15 +2,16 @@ package skritayaUgrozaPartTwo;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
-        System.out.println("May the Force be with you.");
+        String s = "Anakin ";
+        System.out.print(s);
+//        System.out.println("how are you? ");
+//        System.out.println("I am ");
+//        System.out.println("glad ");
+//        System.out.print("to see you.");
+//        System.out.println("This Lightsaber ");
+        System.out.print("is ");
+//        System.out.print("Your");
+        System.out.print("a hero");
+        System.out.println("!");
     }
 }
