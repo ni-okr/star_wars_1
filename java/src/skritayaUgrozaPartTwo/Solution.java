09f5299adfd7454907886fe103d0da7506d4ad80
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 13:37:16 +0300

    Эпизод 1: Скрытая угроза. Часть 2 последняя воля

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,9 +1,13 @@
 package skritayaUgrozaPartTwo;
 
 public class Solution {
+    public static int a = 1;
+    public static int b = 3;
+    public static int c = 9;
+    public static int d = 27;
+
     public static void main(String[] args) {
-        String darth = "Mol";
-        String text = "Darth " + darth + "!";
-        System.out.println(text);
+        int result = -a + b - c + d;
+        System.out.println(result);
     }
 }
