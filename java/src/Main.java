d15f784adab5243bbbc005ac2fba532eb8ad0bb4
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 12:27:39 +0300

    Эпизод 1: скрытая угроза. Часть 1 Блокада планеты Набу

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Destroy Droidekas");
-        System.out.println("Destroy Droidekas");
-        System.out.println("Destroy Droidekas");
-        System.out.println("Destroy Droidekas");
-        System.out.println("Destroy Droidekas");
+        System.out.println(Integer.toString(4-3));
     }
 }
