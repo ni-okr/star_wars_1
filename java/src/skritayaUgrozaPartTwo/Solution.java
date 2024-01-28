379189de43e8c7b111bc83f3e6591072fd05c549
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 13:43:13 +0300

    Эпизод 1: Скрытая угроза. Часть 2 Прощание с Квай-Гоном

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -2,10 +2,16 @@ package skritayaUgrozaPartTwo;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(sqr(5));
-    }
-
-    public static int sqr(int a) {
-        return a * a;
+//        int a = 1;
+        double b = 1.5;
+        double c = b + 1.5;
+//        int d = a + 12;
+//        double e = 12.3;
+//        String s = "Luke, " + a;
+        String s1 = "Twice ";
+//        String s2 = "a";
+        String s3 = s1 + "the pride, ";
+        String s4 = " the fall.";
+        System.out.println(s3 + c + s4);
     }
 }
