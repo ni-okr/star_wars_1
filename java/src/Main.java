55b4d4060cf51693f564c764a83c02c6c4e80bf9
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 12:25:13 +0300

    Эпизод 1: скрытая угроза. Часть 1 Королевский дворец

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Поздравляю товарища Ким Чен Ына с успешным запуском в японском море!");
-        System.out.println("Поздравляю товарища Ким Чен Ына\n с успешным запуском\n в японском море!");
+        System.out.println("Destroy Droidekas");
+        System.out.println("Destroy Droidekas");
+        System.out.println("Destroy Droidekas");
+        System.out.println("Destroy Droidekas");
+        System.out.println("Destroy Droidekas");
     }
 }
