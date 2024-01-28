ff5923ce2afef70a05b542be7a95bf370b22d0fc
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 12:40:01 +0300

    Эпизод 1: скрытая угроза. Часть 1 Перед гонкой

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("The");
-//        System.out.print("Starship");
-//        System.out.println(" car ");
-        System.out.print(" car");
-        System.out.print(" is ");
-        System.out.print("ready ");
-//        System.out.print(" ready ");
-        System.out.print("for");
-//        System.out.println(" the");
-        System.out.print(" the");
-        System.out.println(" race");
+        System.out.print("It's blue side");
     }
 }
