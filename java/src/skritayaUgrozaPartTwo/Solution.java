7f37395df3c451f5067080ebb239630ee8b93e0a
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-28 15:19:46 +0300

    Эпизод 2: Атака клонов. Часть 2 Камино - родина клонов

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,16 +1,19 @@
 package skritayaUgrozaPartTwo;
 
 public class Solution {
-    public static class Jedi {
-        public String name;
+    public static class Clone {
     }
 
     public static void main(String[] args) {
-        Jedi jedi1 = new Jedi();
-        Jedi jedi2 = new Jedi();
-        Jedi jedi3 = new Jedi();
-        jedi1.name = "Obi-Wan";
-        jedi2.name = "Anakin";
-        jedi3.name = "Joda";
+        Clone clone1 = new Clone();
+        Clone clone2 = new Clone();
+        Clone clone3 = new Clone();
+        Clone clone4 = new Clone();
+        Clone clone5 = new Clone();
+        Clone clone6 = new Clone();
+        Clone clone7 = new Clone();
+        Clone clone8 = new Clone();
+        Clone clone9;
+        Clone clone10;
     }
 }
