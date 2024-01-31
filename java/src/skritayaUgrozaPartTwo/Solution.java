95d82469342a51177ec129fde5e51a3a43c80407
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-31 13:52:49 +0300

    Эпизод 2: Атака клонов. Часть 2 Битва с Графом Дуку

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,21 +1,18 @@
 package skritayaUgrozaPartTwo;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Solution {
+    public static int sumDigitsInNumber(int number){
+        return (number / 100 + number / 10 % 10 + number % 10);
+    }
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        String phrase = scanner.nextLine();
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
-        System.out.println(phrase);
+        int number = scanner.nextInt();
+
+        System.out.println(sumDigitsInNumber(322));
     }
+
 }
