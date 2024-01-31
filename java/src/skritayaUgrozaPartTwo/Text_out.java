eb55c777b4f0014c6bad5d5b1c2af32a826aa88b
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-31 10:38:58 +0300

    Эпизод 2: Атака клонов. Часть 2 План раскрыт

---
+++ java/src/skritayaUgrozaPartTwo/Text_out.java
@@ -0,0 +1,13 @@
+package skritayaUgrozaPartTwo;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Text_out {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int digit = scanner.nextInt();
+
+        System.out.println(digit);
+    }
+}
