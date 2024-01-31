27186caaa48fe9719eb9daa670c8f5e5dec4ce0e
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-31 13:47:24 +0300

    Эпизод 2: Атака клонов. Часть 2 Отступление

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,22 +1,21 @@
 package skritayaUgrozaPartTwo;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-//        читаем что пишем с клавы
-//        тут объявим переменную в которую будем заносить все что пишем
-        InputStream inputStream = System.in;
-
-//        конструктор inputStreamReader() из класса Reader что бы перевести информацию из байтов в символы
-        Reader inputStreamReader = new InputStreamReader(inputStream);
-//        конструктор bufferedReader() класса BufferedReader для увеличения производительности чтения из потока
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-//        присваиваем написанное с клавы в переменную, считывая строку методом readLine() класса bufferedReader
-        String name = bufferedReader.readLine();
-        String count = bufferedReader.readLine();
-
-        System.out.println("Master " + name + " is here " + count + " jedis");
+        Scanner scanner = new Scanner(System.in);
+        String phrase = scanner.nextLine();
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
+        System.out.println(phrase);
     }
 }
