530746a50383cad905fa300f079747d96baf2518
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-31 10:35:52 +0300

    Эпизод 2: Атака клонов. Часть 2 Гнев Скайуокера

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -1,12 +1,9 @@
 package skritayaUgrozaPartTwo;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         InputStream inputStream = System.in;
         Reader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
