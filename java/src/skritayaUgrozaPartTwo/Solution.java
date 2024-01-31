f58eb009b17e3f400f0adeb4e18a5df8ed4c70d1
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-31 13:36:28 +0300

    Эпизод 2: Атака клонов. Часть 2 В плену

--- java/src/skritayaUgrozaPartTwo/Solution.java
+++ java/src/skritayaUgrozaPartTwo/Solution.java
@@ -4,11 +4,18 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+//        читаем что пишем с клавы
+//        тут объявим переменную в которую будем заносить все что пишем
         InputStream inputStream = System.in;
-        Reader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String phrase = bufferedReader.readLine();
 
-        System.out.println(phrase);
+//        конструктор inputStreamReader() из класса Reader что бы перевести информацию из байтов в символы
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+//        конструктор bufferedReader() класса BufferedReader для увеличения производительности чтения из потока
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+//        присваиваем написанное с клавы в переменную, считывая строку методом readLine() класса bufferedReader
+        String name = bufferedReader.readLine();
+
+        System.out.println(name + " is a header of the Senate");
     }
 }
