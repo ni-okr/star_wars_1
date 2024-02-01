8dbf8a95ce9491cfb4f728d6c2aca23155dfedd8
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-01 10:46:48 +0300

    Эпизод 2: Месть Ситхов. Часть 1 Темная сторона Силы

---
+++ java/src/epizodTwoMestSithovPartOne/DarkSideOfPower.java
@@ -0,0 +1,18 @@
+package epizodTwoMestSithovPartOne;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DarkSideOfPower {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int numb = Integer.parseInt(reader.readLine());
+        if (numb < 0)
+            System.out.println("it's Dark side");
+        else if (numb == 0)
+            System.out.println("it's balance");
+        else
+            System.out.println("it's Light side");
+    }
+}
