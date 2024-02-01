25acd705f9e923fbc2f7bd6780b4df9979d2a809
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-01 18:40:41 +0300

    Эпизод 3: Месть Ситхов. Часть 2 В доме Падме

---
+++ java/src/epizodThree/mestSithov/partTwo/VDomePadme.java
@@ -0,0 +1,21 @@
+package epizodThree.mestSithov.partTwo;
+
+public class VDomePadme {
+    public class Droid {
+        String name;
+        String model;
+
+        public Droid (String name){
+            this.name = name;
+        }
+
+        public Droid (String name, String model){
+            this.name = name;
+            this.model = model;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
