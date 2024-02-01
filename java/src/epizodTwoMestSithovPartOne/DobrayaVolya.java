109f516f7379c6bd9c610915cf299d56991e6dd5
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-01 12:38:27 +0300

    Эпизод 2: Месть Ситхов. Часть 1 Добрая воля

---
+++ java/src/epizodTwoMestSithovPartOne/DobrayaVolya.java
@@ -0,0 +1,16 @@
+package epizodTwoMestSithovPartOne;
+
+public class DobrayaVolya {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println("I'm so happy");
+            i++;
+        }
+        System.out.println();
+        System.out.println();
+        for (int j = 0; j <= 10; j++) {
+            System.out.println("I'm so happy");
+        }
+    }
+}
