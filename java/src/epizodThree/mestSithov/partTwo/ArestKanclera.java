6e8089ef1ddc204bdd7beaef5ce11f7f55b4119d
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-01 16:23:18 +0300

    Эпизод 3: Месть Ситхов. Часть 2 Арест Канцлера

---
+++ java/src/epizodThree/mestSithov/partTwo/ArestKanclera.java
@@ -0,0 +1,28 @@
+package epizodThree.mestSithov.partTwo;
+
+public class ArestKanclera {
+    public class Sith{
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+}
