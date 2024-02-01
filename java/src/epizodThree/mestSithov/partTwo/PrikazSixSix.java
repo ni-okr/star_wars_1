3c59c5ae55d5b36dd39868b569e5cb678262b883
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-01 16:34:12 +0300

    Эпизод 3: Месть Ситхов. Часть 2 Приказ 66

--- java/src/epizodThree/mestSithov/partTwo/PrikazSixSix.java
+++ java/src/epizodThree/mestSithov/partTwo/PrikazSixSix.java
@@ -1,4 +1,23 @@
 package epizodThree.mestSithov.partTwo;
 
 public class PrikazSixSix {
+    public static class Jedi {
+        private String name;
+        private int age;
+        private int height;
+        private String race;
+
+        public Jedi(String name, int age, int height, String race) {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+            this.race = race;
+        }
+    }
+
+    public static void main(String[] args) {
+        Jedi yoda = new Jedi("Yoda", 900, 66, "unknown");
+        Jedi jinn = new Jedi("Qui-Gon", 60, 193, "human");
+        Jedi kenobi = new Jedi("Obi-Wan", 57, 182, "human");
+    }
 }
