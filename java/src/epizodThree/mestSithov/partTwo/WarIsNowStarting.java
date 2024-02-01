eedd925d162111f039eff6d6bb9617b748cf24fa
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-01 16:22:58 +0300

    Эпизод 3: Месть Ситхов. Часть 2 Война только начинается

---
+++ java/src/epizodThree/mestSithov/partTwo/WarIsNowStarting.java
@@ -0,0 +1,14 @@
+package epizodThree.mestSithov.partTwo;
+
+public class WarIsNowStarting {
+    public class Sith {
+
+        public String name;
+        public int age;
+        public String race;
+        public int height;
+    }
+
+    public static void main(String[] args) {
+    }
+}
