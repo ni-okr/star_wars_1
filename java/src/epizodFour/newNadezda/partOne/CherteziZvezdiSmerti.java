910e76a410b6c4f168fe2bd49ffaf3d327391641
Роман Новиков <chVeZcv[jALy\n:(>
2024-02-02 13:09:54 +0300

    Эпизод 4: Новая надежда. Часть 1 Чертежи Звезды Смерти

---
+++ java/src/epizodFour/newNadezda/partOne/CherteziZvezdiSmerti.java
@@ -0,0 +1,11 @@
+package epizodFour.newNadezda.partOne;
+
+public class CherteziZvezdiSmerti {
+    protected void finalize() throws Throwable {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
